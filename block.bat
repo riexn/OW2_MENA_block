4587043862167b106d44402eeb846c6212e06784
riexn <37214468+riexn@users.noreply.github.com>
2022-10-07 18:52:31 +0400

    fixed IP ranges for MENA addresses

--- block.bat
+++ block.bat
@@ -1,6 +1,6 @@
-netsh advfirewall firewall add rule name="BLOCK_OW2_MENA_A" dir=in action=block remoteip="157.185.0.0,157.185.255.255"
+netsh advfirewall firewall add rule name="BLOCK_OW2_MENA_A" dir=in action=block remoteip="15.185.0.0,15.185.255.255"
 netsh advfirewall firewall add rule name="BLOCK_OW2_MENA_B" dir=in action=block remoteip="157.175.0.0,157.175.255.255"
-netsh advfirewall firewall add rule name="BLOCK_OW2_MENA_C" dir=in action=block remoteip="157.184.0.0,157.184.255.255"
+netsh advfirewall firewall add rule name="BLOCK_OW2_MENA_C" dir=in action=block remoteip="15.184.0.0,15.184.255.255"
 netsh advfirewall firewall add rule name="BLOCK_OW2_MENA_D" dir=in action=block remoteip="35.198.0.0,35.198.255.255"
 netsh advfirewall firewall add rule name="BLOCK_OW2_MENA_E" dir=in action=block remoteip="35.94.0.0,35.94.255.255"
 netsh advfirewall firewall add rule name="BLOCK_OW2_MENA_F" dir=in action=block remoteip="35.142.0.0,35.142.255.255"
